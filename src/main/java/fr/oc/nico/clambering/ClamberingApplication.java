37710a2a8f99c67c0d48aaa46da37bccf37c03de
nlco <nicolas-lejeune@live.fr>
2019-07-01 17:38:04 +0200

    init : created by using Spring Initializr (https://start.spring.io/)

---
+++ src/main/java/fr/oc/nico/clambering/ClamberingApplication.java
@@ -0,0 +1,13 @@
+package fr.oc.nico.clambering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClamberingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClamberingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/fr/oc/nico/clambering/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fr.oc.nico.clambering;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ClamberingApplication.class);
+	}
+
+}

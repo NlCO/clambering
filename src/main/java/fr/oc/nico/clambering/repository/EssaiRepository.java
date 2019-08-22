24c41e0e653e8b8fc64bb55bdeb14dfe8aee318c
nlco <nicolas-lejeune@live.fr>
2019-08-22 23:41:56 +0200

    clean : remove Essai

--- src/main/java/fr/oc/nico/clambering/repository/EssaiRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.oc.nico.clambering.repository;
-
-import fr.oc.nico.clambering.entity.Essai;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EssaiRepository extends JpaRepository<Essai, Integer> {
-}

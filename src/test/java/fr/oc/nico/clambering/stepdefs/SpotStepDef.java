587f46fba5f6b9505c1ec5cb9d66c61515e569d8
nlco <nicolas-lejeune@live.fr>
2019-09-14 22:21:54 +0200

    Add(Tests) : Add cucumber + Spot test

---
+++ src/test/java/fr/oc/nico/clambering/stepdefs/SpotStepDef.java
@@ -0,0 +1,46 @@
+package fr.oc.nico.clambering.stepdefs;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import fr.oc.nico.clambering.CucumberTest;
+import fr.oc.nico.clambering.model.Spot;
+import fr.oc.nico.clambering.repository.SpotRepository;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityNotFoundException;
+
+
+public class SpotStepDef extends CucumberTest {
+
+    @Autowired
+    SpotRepository spotRepository;
+
+    private Spot spot;
+
+    @When("Je consulte les informations du spot ayant l'(\\d+)$")
+    public void jeConsulteLesInformationsDuSpotAyantLId(int id) {
+        spot = spotRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Le spot " + id + " n'exite pas"));
+    }
+
+    @Then("Sa cotation max est (.+)$")
+    public void saCotationMaxEstCotation(String cotation) {
+        Assert.assertEquals(cotation,spot.getCotationMax());
+    }
+
+    @Then("Sa cotation min est (.+)$")
+    public void saCotationMinEstCotation(String cotation) {
+        Assert.assertEquals(cotation,spot.getCotationMin());
+    }
+
+    @Then("Sa hauteur max est {float}")
+    public void saHauteurMaxEstHauteur(Float hauteur) {
+        Assert.assertEquals(hauteur,spot.getHauteurMax());
+    }
+
+    @Then("Sa hauteur min est {float}")
+    public void saHauteurMinEstHauteur(Float hauteur) {
+        Assert.assertEquals(hauteur,spot.getHauteurMin());
+    }
+}
---
+++ src/test/resources/features/spot.feature
@@ -0,0 +1,50 @@
+Feature: Spot
+
+  En tant qu'utilisateur je souhaite obtenir des informations sur les spots.
+
+  Background:
+    Un jeu de test est chargé.
+
+    Scenario Outline: Cotation Max d'un spot
+      When Je consulte les informations du spot ayant l'<id>
+      Then Sa cotation max est <cotation>
+
+      Examples:
+        |id|cotation|
+        |11|9a      |
+        |6 |9c      |
+        |19|8a      |
+        |3 |8c      |
+
+    Scenario Outline: Cotation Min d'un spot
+      When Je consulte les informations du spot ayant l'<id>
+      Then Sa cotation min est <cotation>
+
+      Examples:
+        |id|cotation|
+        |3 |6a      |
+        |1 |4       |
+        |7 |3       |
+        |4 |5c      |
+
+    Scenario Outline: Hauteur Max d'un spot
+      When Je consulte les informations du spot ayant l'<id>
+      Then Sa hauteur max est <hauteur>
+
+      Examples:
+        |id|hauteur|
+        |2 |52.83  |
+        |4 |20.18  |
+        |12|36.65  |
+        |20|21.29  |
+
+    Scenario Outline: Hauteur Min d'un spot
+      When Je consulte les informations du spot ayant l'<id>
+      Then Sa hauteur min est <hauteur>
+
+      Examples:
+        |id|hauteur|
+        |2 |19.39  |
+        |4 |20.18  |
+        |12|18.27  |
+        |20|16.66  |

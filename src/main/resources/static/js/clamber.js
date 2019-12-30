5a20fdc8835af0d015a4eeb2339c61fa9c0f25c6
nlco <nicolas-lejeune@live.fr>
2019-12-30 14:26:40 +0100

    refacto(): refacto generating GPS Map

--- src/main/resources/static/js/clamber.js
+++ src/main/resources/static/js/clamber.js
@@ -8,3 +8,19 @@ function toggleFilter(e){
 $('#collapseFilterForm').on('hidden.bs.collapse', toggleFilter);
 $('#collapseFilterForm').on('shown.bs.collapse', toggleFilter);
 
+/* Affichage Map*/
+function affichageMap(lat,lon) {
+    var mymap = L.map('mapid').setView([lat, lon], 7);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.outdoors'
+    }).addTo(mymap);
+
+    L.marker([lat, lon]).addTo(mymap);
+    L.popup();
+}
+
